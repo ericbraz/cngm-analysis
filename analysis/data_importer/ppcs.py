049769a7a21524f3f16ffd47f60a3afb6cc0c45d
ericbraz <erpdl2@gmail.com>
2023-11-14 21:15:02 -0300

    Instantiation fixed

--- analysis/data_importer/ppcs.py
+++ analysis/data_importer/ppcs.py
@@ -1,6 +1,7 @@
 from config import PPCS_URL
+from .base import BaseDataFetcher
 
-class Ppcs:
+class Ppcs(BaseDataFetcher):
 
     def __init__(self):
         super().__init__(PPCS_URL, "ppcs.json")
